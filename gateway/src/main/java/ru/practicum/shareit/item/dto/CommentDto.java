929d953dafa11fab5eddf821714049b1f5c32133
TimoshenkoAleksey <timoshenkolexa2022@yandex.ru>
2023-08-12 20:17:54 +0300

    fix: исправил метод в классе gateway/ItemClient, третий коммит шестнадцатого спринта

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
@@ -16,6 +15,5 @@ public class CommentDto {
     @Size(max = 1024)
     private String text;
     private String authorName;
-    @NotNull
     private LocalDateTime created;
 }
--- gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -3,15 +3,20 @@ package ru.practicum.shareit.item.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 @Builder
 public class ItemDto {
     private Long id;
+    @NotBlank
     private String name;
-    @Size(max = 250)
+    @NotBlank
+    @Size(max = 256)
     private String description;
+    @NotNull
     private Boolean available;
     private Long requestId;
 }
